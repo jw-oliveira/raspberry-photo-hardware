de8bb018d078341369063038d18a26e1710d5f56
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2024-01-16 15:02:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from datetime import datetime
 def capture_image(image_name):
     with picamera.PiCamera() as camera:
         time.sleep(0.5)
-        camera.capture(f'/home/jorge/Pictures/camera_test/{image_name}.jpg')
+        camera.capture(f'/home/jorge/Pictures/camera_test/{image_name}.jpg', quality=60)
 
 
 while True:
     label = input(str())
-    caption = f'Pedido: {label[0:5]} \nCaixa: {label[-2:]}'
+    caption = f'Pedido: {label[0:6]} \nCaixa: {label[-2:]}'
     print(f'Etiqueta Capturada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
     capture_image(label)
     print(f'Imagem Capturada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
