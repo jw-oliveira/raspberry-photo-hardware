4d56c0c735091fab7007160725143dc12bcc3508
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2024-01-16 15:00:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ def capture_image(image_name):
 
 while True:
     label = input(str())
+    caption = f'Pedido: {label[0:5]} \nCaixa: {label[-2:]}'
     print(f'Etiqueta Capturada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
     capture_image(label)
     print(f'Imagem Capturada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-    enviar_imagem(f'/home/jorge/Pictures/camera_test/{label}.jpg', label)
+    enviar_imagem(f'/home/jorge/Pictures/camera_test/{label}.jpg', caption)
     print(f'Imagem enviada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
