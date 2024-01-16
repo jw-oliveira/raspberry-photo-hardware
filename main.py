9033778928c1b886b1534fc00e62bcae25dd48f1
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2024-01-16 14:04:25 -0300

    feat-add-file-extension

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import picamera
 def capture_image(image_name):
     with picamera.PiCamera() as camera:
         time.sleep(0.5)
-        camera.capture(f'/home/jorge/Pictures/camera_test/{image_name}')
+        camera.capture(f'/home/jorge/Pictures/camera_test/{image_name}.jpg')
 
 
 label = input(str())
