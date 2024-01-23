2aab88f06ef815eb60feb0550658dc8a3a258d02
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2024-01-23 13:18:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from telegram_sender import enviar_imagem
 from datetime import datetime
 
 
-def capture_image(resolution=(1920, 1080)):
+def capture_image(image_name):
     with picamera.PiCamera() as camera:
+        camera.resolution = (1920, 1080)
         time.sleep(0.5)
         stream = io.BytesIO()
         camera.capture(stream, format='jpeg', quality=10)
         image_binary = stream.getvalue()
         base64_data = base64.b64encode(image_binary).decode('utf-8')
-        return base64_data
+        return image_name, base64_data
 
 
-print(capture_image())
+image_name_input = input(str())
+
+print(capture_image(image_name_input)
+
 print(f'Imagem Capturada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
