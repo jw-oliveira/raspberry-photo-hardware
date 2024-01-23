1667f212fd9ec3761129b9399299e37fb25364b6
Jorge Willian de Oliveira <91788583+jw-oliveira@users.noreply.github.com>
2024-01-23 13:01:13 -0300

    feat: add base64 converter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import picamera
+import base64
+import io
 from telegram_sender import enviar_imagem
 from datetime import datetime
 
@@ -7,14 +9,18 @@ from datetime import datetime
 def capture_image(image_name):
     with picamera.PiCamera() as camera:
         time.sleep(0.5)
-        camera.capture(f'/home/jorge/Pictures/camera_test/{image_name}.jpg', quality=10)
+        # camera.capture(f'/home/jorge/Pictures/camera_test/{image_name}.jpg', quality=10)
+        camera.capture(stream, format='jpeg', quality=10)
+        image_binary = stream.getvalue()
+        base64_data = base64.b64encode(image_binary).decode('utf-8')
+        return base64_data
 
 
 while True:
     label = input(str())
     caption = f'Pedido: {label[0:6]} \nCaixa: {label[-2:]}'
     print(f'Etiqueta Capturada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-    capture_image(label)
+    print(capture_image)
     print(f'Imagem Capturada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-    enviar_imagem(f'/home/jorge/Pictures/camera_test/{label}.jpg', caption)
-    print(f'Imagem enviada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+    # enviar_imagem(f'/home/jorge/Pictures/camera_test/{label}.jpg', caption)
+    # print(f'Imagem enviada - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
